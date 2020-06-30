d33cba8a34539c3c87b4657ad9a7c000c91eb8d0
anathayna <anathaynafranca@gmail.com>
2020-06-29 22:17:32 -0300

    starting project

--- memorize/memorize/ContentView.swift
+++ memorize/memorize/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👽")
+            })
+            .foregroundColor(.orange)
+            .padding()
+            .font(Font.largeTitle)
     }
 }
 
