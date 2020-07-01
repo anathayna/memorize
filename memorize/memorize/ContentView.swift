c944fc90c67fce4d5ed3d38373b23ce4d4c1441e
anathayna <anathaynafranca@gmail.com>
2020-07-01 20:37:47 -0300

    clean code

--- memorize/memorize/ContentView.swift
+++ memorize/memorize/ContentView.swift
@@ -10,22 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(spacing: 10, content: {
-            ForEach(0..<4, content: {_ in
-                ZStack(content: {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text("👽")
-                })
-            })
-        })
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
             .foregroundColor(.orange)
             .padding()
             .font(Font.largeTitle)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct CardView: View {
+    var body: some View {
+        ZStack() {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👽")
+        }
+    }
+}
+
+struct ContentViewPreviews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
