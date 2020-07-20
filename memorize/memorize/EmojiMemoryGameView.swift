18b752f2ffb9bf78fe4b6515e92a173d4594a81f
anathayna <anathaynafranca@gmail.com>
2020-07-20 20:43:16 -0300

    adding drawing constants

--- memorize/memorize/EmojiMemoryGameView.swift
+++ memorize/memorize/EmojiMemoryGameView.swift
@@ -30,16 +30,18 @@ struct CardView: View {
         GeometryReader { geometry in
             ZStack {
                 if self.card.isFaceUp {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    RoundedRectangle(cornerRadius: self.cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: self.cornerRadius).stroke(lineWidth: self.edgeLineWidth)
                     Text(self.card.content)
                 } else {
-                    RoundedRectangle(cornerRadius: 10.0).fill()
+                    RoundedRectangle(cornerRadius: self.cornerRadius).fill()
                 }
             }
-            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.75))
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * self.fontScaleFactor))
         }
     }
     
-    
+    let cornerRadius: CGFloat = 10
+    let edgeLineWidth: CGFloat = 3
+    let fontScaleFactor: CGFloat = 0.75
 }
