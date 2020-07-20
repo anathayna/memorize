5b93803977c7760bf532bd6a7d1f08119ce87166
anathayna <anathaynafranca@gmail.com>
2020-07-20 20:31:52 -0300

    adding geometry reader

--- memorize/memorize/EmojiMemoryGameView.swift
+++ memorize/memorize/EmojiMemoryGameView.swift
@@ -28,14 +28,16 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     var card: MemoryGame<String>.Card
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+        GeometryReader(content: { geometry in
+            ZStack {
+                if self.card.isFaceUp {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text(self.card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: 10.0).fill()
+                }
             }
-        }
+        })
     }
 }
