366d0cdfb0d389aa9613644658bb2817e2a8b5e0
ana thayna <anathaynafranca@gmail.com>
2020-12-21 22:17:54 -0300

    add cardify struc

---
+++ memorize/memorize/View/Cardify.swift
@@ -0,0 +1,34 @@
+//
+//  Cardify.swift
+//  memorize
+//
+//  Created by ana thayna on 21/12/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+    }
+    
+    private let cornerRadius: CGFloat = 10
+    private let edgeLineWidth: CGFloat = 3
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
