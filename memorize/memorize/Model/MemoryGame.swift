b5fb1df12418939f5ece73286f6d4ba966a09051
ana thayna <anathaynafranca@gmail.com>
2021-01-11 22:15:09 -0300

    finish pie animation

--- memorize/memorize/Model/MemoryGame.swift
+++ memorize/memorize/Model/MemoryGame.swift
@@ -10,6 +10,9 @@ import Foundation
 
 struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
+    var theme: Theme
+    var score = 0
+    var seenCards = Set<Int>()
     
     private var indexOneAndOnlyFaceUpCard: Int? {
         get { return cards.indices.filter { cards[$0].isFaceUp }.only }
@@ -22,11 +25,21 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     mutating func choose(card: Card) {
         print("card choose: \(card)")
-        if let chosenIndex = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMached {
+        if let chosenIndex = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
             if let potentialMatchedIndex = indexOneAndOnlyFaceUpCard {
                 if cards[chosenIndex].content == cards[potentialMatchedIndex].content {
-                    cards[chosenIndex].isMached = true
-                    cards[potentialMatchedIndex].isMached = true
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchedIndex].isMatched = true
+                    score += 2
+                } else {
+                    if seenCards.contains(cards[chosenIndex].id) {
+                        score -= 1
+                    }
+                    if seenCards.contains(cards[potentialMatchedIndex].id) {
+                        score -= 1
+                    }
+                    seenCards.insert(cards[chosenIndex].id)
+                    seenCards.insert(cards[potentialMatchedIndex].id)
                 }
                 self.cards[chosenIndex].isFaceUp = true
             } else {
@@ -35,23 +48,76 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
-    init(numberOfPairsOfCards: Int, cardContentfactory: (Int) -> CardContent) {
+    init(theme: Theme, cardContentfactory: (Int) -> CardContent) {
         cards = Array<Card>()
         
-        for pairIndex in 0..<numberOfPairsOfCards {
+        for pairIndex in 0..<theme.numberOfPairsOfCards {
             let content = cardContentfactory(pairIndex)
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2+1))
         }
         
         cards.shuffle()
+        self.theme = theme
     }
     
     struct Card : Identifiable {
-        var isFaceUp: Bool = false
-        var isMached: Bool = false
+        var isFaceUp: Bool = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        
+        var isMatched: Bool = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
+        
+        var pastFaceUpTime: TimeInterval = 0
+        var bonusTimeLimit: TimeInterval = 6
+        var lastFaceUpDate: Date?
         var content: CardContent
         var id: Int
+        
+        
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+        }
+        
+        var hasEarnedBonus: Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
     }
     
 }
